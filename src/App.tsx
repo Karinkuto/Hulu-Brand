c2bff7634ff4e5d801c6d66c0f69a1698ee034e8
Karinkuto <sanyiiamen@gmail.com>
2024-10-16 12:20:47 +0300

    removed the admin side functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,8 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 import ProductDetailsPage from './pages/ProductDetailsPage';
-import AdminDashboard from './pages/admin/Dashboard';
-import AdminProducts from './pages/admin/Products';
-import AdminOrders from './pages/admin/Orders';
 import AuthPage from './pages/AuthPage';
 import { useAuthStore } from './stores/authStore';
-import UsersPage from './pages/admin/UsersPage'; // Updated import
 import { Cart } from './components/Cart';
 import { Notifications } from './components/Notifications';
 import TransactionsPage from './pages/TransactionsPage';
@@ -34,10 +30,6 @@ export default function App() {
             <Route path="/notifications" element={<Notifications />} />
             {isAuthenticated && isAdmin && (
               <>
-                <Route path="/admin/dashboard" element={<AdminDashboard />} />
-                <Route path="/admin/products" element={<AdminProducts />} />
-                <Route path="/admin/orders" element={<AdminOrders />} />
-                <Route path="/admin/users" element={<UsersPage />} />
                 <Route path="/transactions" element={<TransactionsPage />} />
               </>
             )}
